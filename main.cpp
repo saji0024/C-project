52f81a1f3447bdc5f295c676e96d26d75cc0d9a4
saji0024 <sajileyto024@gmail.com>
2025-06-01 15:31:42 +0330

    Receive a number and print numbers smaller than it

--- main.cpp
+++ main.cpp
@@ -1,55 +1,14 @@
 #include <iostream>
-#include <limits> 
-#include <windows.h>
 using namespace std;
+int main()
+ {
+  int a;
+  cout<<"enter the number=\t";
+  cin>>a;
+  for(a;a>0;a--)
+  
+    cout<<a<<"\t";
+  
 
-int main() {
-    int number;
-    int largest = numeric_limits<int>::min(); 
-    int sum = 0;
-
-    cout << "Enter some numbers(To end, enter a negative number.):" << endl;
-
-    while (true) {
-        cout << "numbers: ";
-        cin >> number;
-        {
-        	system("color 9");
-        	system("color A");
-        	system("color B");
-        	system("color C");
-        	system("color D");
-        	system("color E");
-        	system("color F");
-        	system("color 9");
-        	system("color A");
-        	system("color B");
-        	system("color C");
-        	system("color D");
-        	system("color E");
-        	system("color F");
-        	system("color 9");
-        	system("color A");
-        	system("color B");
-        	system("color C");
-        	system("color D");
-        	system("color E");
-        	system("color F");
-		}
-
-        if (number < 0) { 
-            break;
-        }
-
-        sum += number; 
-
-        if (number > largest) { 
-            largest = number;
-        }
-    }
-
-    cout << "The largest number: " << largest << endl;
-    cout << "Sum of numbers: " << sum << endl;
-
-    return 0;
+  return 0;
 }
